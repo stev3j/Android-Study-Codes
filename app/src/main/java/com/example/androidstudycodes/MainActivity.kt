34ba545cb0104f7a0be80d1d32c329c51837bbce
stev3j <mygajeong@gmail.com>
2023-05-06 20:37:50 +0900

    ✨ :: LiveData Simple Example
    mvc로 구현한 livedata 예시

--- app/src/main/java/com/example/androidstudycodes/MainActivity.kt
+++ app/src/main/java/com/example/androidstudycodes/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.androidstudycodes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.example.androidstudycodes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding : ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val tvCount = MutableLiveData<String>().apply { value = "Hello, ${count++}" }
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        // tvCount가 바뀔 때마다 tvCount.text를 tvCount로 변환시킴
+        tvCount.observe(this) { tvCount ->
+            binding.tvCount.text = tvCount
+        }
+
+        binding.btnPlus.setOnClickListener {
+            tvCount.value = "Hello, ${count++}"
+        }
     }
 }
