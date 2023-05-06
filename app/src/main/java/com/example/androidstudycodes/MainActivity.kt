1e069f7e161f4c4df41bea2bdf76f3024378ca57
stev3j <mygajeong@gmail.com>
2023-05-06 21:27:32 +0900

    ✨ :: LiveData with ViewModel Example
    viewModel로써 코드가 정리됨

--- app/src/main/java/com/example/androidstudycodes/MainActivity.kt
+++ app/src/main/java/com/example/androidstudycodes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidstudycodes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.example.androidstudycodes.databinding.ActivityMainBinding
@@ -10,21 +11,19 @@ import com.example.androidstudycodes.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private val binding : ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
-    private val tvCount = MutableLiveData<String>().apply { value = "Hello, ${count++}" }
-    private var count = 0
+    private val viewModel : MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         // tvCount가 바뀔 때마다 tvCount.text를 tvCount로 변환시킴
-        tvCount.observe(this) { tvCount ->
-            binding.tvCount.text = tvCount
+        viewModel.countText.observe(this) { countText ->
+            binding.tvCount.text = countText
         }
 
         binding.btnPlus.setOnClickListener {
-            tvCount.value = "Hello, ${count++}"
+            viewModel.plusValue()
         }
     }
 }
---
+++ app/src/main/java/com/example/androidstudycodes/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.androidstudycodes
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private var count = 0
+    private var _countText = MutableLiveData<String>().apply { value = "Hello, ${count++}" }
+    val countText : LiveData<String> = _countText
+
+    fun plusValue() {
+        _countText.value = "Hello, ${count++}"
+    }
+}
