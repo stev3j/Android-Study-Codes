6c81dc5b49b975bf78f9501a8e16240eb22712f1
stev3j <mygajeong@gmail.com>
2023-06-15 11:02:29 +0900

    ✨ :: DI Study
    의존성 주입의 정말 간단한 예시

---
+++ app/src/main/java/com/example/androidstudycodes/di_practice/App.kt
@@ -0,0 +1,7 @@
+package com.example.androidstudycodes.di_practice
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() { }
---
+++ app/src/main/java/com/example/androidstudycodes/di_practice/DiTestActivity.kt
@@ -0,0 +1,27 @@
+package com.example.androidstudycodes.di_practice
+
+import android.nfc.Tag
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.androidstudycodes.R
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class DiTestActivity : AppCompatActivity() {
+
+    private val TAG = "DI Test"
+
+    @Inject
+    lateinit var testString: String
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_di_test)
+
+//        Log.d(TAG, testString)
+        println(testString)
+    }
+}
